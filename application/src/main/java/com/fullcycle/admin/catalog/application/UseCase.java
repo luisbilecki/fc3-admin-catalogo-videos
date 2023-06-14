31d711a65e282071f3b3e0e8c6d6a2f09a4ccd32
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-06-14 15:11:33 -0300

    feat: creating category entity with tdd

--- application/src/main/java/com/fullcycle/admin/catalog/application/UseCase.java
+++ application/src/main/java/com/fullcycle/admin/catalog/application/UseCase.java
@@ -1,7 +1,5 @@
 package com.fullcycle.admin.catalog.application;
 
-import com.fullcycle.admin.catalog.domain.Category;
-
 public class UseCase {
 
     public Category execute() {
--- domain/src/test/java/com/fullcycle/admin/catalog/domain/CategoryTest.java
+++ domain/src/test/java/com/fullcycle/admin/catalog/domain/CategoryTest.java
@@ -1,12 +1,25 @@
 package com.fullcycle.admin.catalog.domain;
 
+import com.fullcycle.admin.catalog.domain.category.Category;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CategoryTest {
 
     @Test
-    public void testNewCategory() {
-        Assertions.assertNotNull(new Category());
+    public void givenAValidParams_whenCallNewCategory_thenInstantiateNewCategory() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
     }
 }
