4e7a1771849461af2d5a0aba3c34daa9b540c188
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-06-14 15:46:23 -0300

    feat: category to aggregate root

--- domain/src/main/java/com/fullcycle/admin/catalog/domain/category/Category.kt
+++ domain/src/main/java/com/fullcycle/admin/catalog/domain/category/Category.kt
@@ -1,21 +1,22 @@
 package com.fullcycle.admin.catalog.domain.category
 
+import com.fullcycle.admin.catalog.domain.AggregateRoot
 import java.time.Instant
 import java.util.*
 
 class Category private constructor(
-        val id: String,
+        val id: CategoryID,
         val name: String,
         val description: String,
         val isActive: Boolean,
         val createdAt: Instant,
         val updatedAt: Instant,
         val deletedAt: Instant?
-) {
+) : AggregateRoot<CategoryID>(id) {
 
     companion object {
         fun newCategory(aName: String, aDescription: String, aIsActive: Boolean): Category {
-            val id = UUID.randomUUID().toString()
+            val id = CategoryID.unique()
             val now = Instant.now()
             return Category(id, aName, aDescription, aIsActive, now, now, null)
         }
---
+++ domain/src/main/java/com/fullcycle/admin/catalog/domain/category/CategoryID.kt
@@ -0,0 +1,30 @@
+package com.fullcycle.admin.catalog.domain.category
+
+import com.fullcycle.admin.catalog.domain.Identifier
+import java.util.*
+
+class CategoryID private constructor(val value: String) : Identifier() {
+
+    companion object {
+        fun unique() = from(UUID.randomUUID())
+
+        fun from(anId: String) = CategoryID(anId)
+
+        fun from(anId: UUID) = CategoryID(anId.toString().lowercase(Locale.getDefault()))
+
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as CategoryID
+
+        return value == other.value
+    }
+
+    override fun hashCode(): Int {
+        return value.hashCode()
+    }
+
+}
