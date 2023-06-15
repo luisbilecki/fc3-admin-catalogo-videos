958bd5f7946caf0c0949d08c71ecc0c75e6d6483
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-06-15 15:14:32 -0300

    feat: add mockito to application

--- application/src/test/java/com/fullcycle/admin/catalog/application/UseCaseTest.kt
+++ application/src/test/java/com/fullcycle/admin/catalog/application/UseCaseTest.kt
@@ -1,12 +1,12 @@
 package com.fullcycle.admin.catalog.application
 
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
 
+@ExtendWith(MockitoExtension::class)
 class UseCaseTest {
     @Test
     fun testCreateUseCase() {
-        Assertions.assertNotNull(UseCase())
-        Assertions.assertNotNull(UseCase().execute())
     }
 }
