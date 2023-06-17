0d080c3f27dd6bad367a0806239ede6a2bdeca12
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-06-17 15:07:52 -0300

    feat: add vavr library

--- application/src/main/java/com/fullcycle/admin/catalog/application/category/create/CreateCategoryUseCase.kt
+++ application/src/main/java/com/fullcycle/admin/catalog/application/category/create/CreateCategoryUseCase.kt
@@ -1,5 +1,7 @@
 package com.fullcycle.admin.catalog.application.category.create
 
 import com.fullcycle.admin.catalog.application.UseCase
+import com.fullcycle.admin.catalog.domain.validation.handler.Notification
+import io.vavr.control.Either
 
-abstract class CreateCategoryUseCase : UseCase<CreateCategoryCommand, CreateCategoryOutput>()
+abstract class CreateCategoryUseCase : UseCase<CreateCategoryCommand, Either<Notification, CreateCategoryOutput>>()
