4d098d84f80fc2c7890d363c793147116a6ea28c
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-06-19 13:49:39 -0300

    feat: add spring boot to infra layer

--- infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/Main.kt
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/Main.kt
@@ -1,11 +1,12 @@
 package com.fullcycle.admin.catalog.infrastructure
 
-import com.fullcycle.admin.catalog.application.UseCase
+import com.fullcycle.admin.catalog.infrastructure.configuration.WebServerConfig
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
-object Main {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        println("Hello world!")
-        println(UseCase().execute())
-    }
+@SpringBootApplication
+open class Main
+
+fun main(args: Array<String>) {
+    SpringApplication.run(WebServerConfig::class.java, *args)
 }
---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/configuration/WebServerConfig.kt
@@ -0,0 +1,8 @@
+package com.fullcycle.admin.catalog.infrastructure.configuration
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan("com.fullcycle.admin.catalog")
+open class WebServerConfig
