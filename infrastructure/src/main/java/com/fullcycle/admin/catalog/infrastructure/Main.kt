4962ca956bce4b522f6a52080b715a7bbdb3465a
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-06-19 13:54:55 -0300

    feat: configure profiles on spring boot

--- infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/Main.kt
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/Main.kt
@@ -3,10 +3,12 @@ package com.fullcycle.admin.catalog.infrastructure
 import com.fullcycle.admin.catalog.infrastructure.configuration.WebServerConfig
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.core.env.AbstractEnvironment
 
 @SpringBootApplication
 open class Main
 
 fun main(args: Array<String>) {
+    System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development")
     SpringApplication.run(WebServerConfig::class.java, *args)
 }
