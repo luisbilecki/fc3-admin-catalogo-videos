f84eb799bf953a39bb6827e050f8c8a0e9d1a325
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-06-20 17:00:42 -0300

    feat: add integration test for category update

--- infrastructure/src/test/java/com/fullcycle/admin/catalog/infrastructure/category/CategoryMySQLGatewayTest.kt
+++ infrastructure/src/test/java/com/fullcycle/admin/catalog/infrastructure/category/CategoryMySQLGatewayTest.kt
@@ -2,6 +2,7 @@ package com.fullcycle.admin.catalog.infrastructure.category
 
 import com.fullcycle.admin.catalog.domain.category.Category
 import com.fullcycle.admin.catalog.infrastructure.MySQLGatewayTest
+import com.fullcycle.admin.catalog.infrastructure.category.persistence.CategoryJpaEntity
 import com.fullcycle.admin.catalog.infrastructure.category.persistence.CategoryRepository
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
@@ -80,6 +81,49 @@ open class CategoryMySQLGatewayTest {
         Assertions.assertEquals(expectedIsActive, actualEntity.isActive)
         Assertions.assertEquals(category.createdAt, actualEntity.createdAt)
         Assertions.assertEquals(category.updatedAt, actualEntity.updatedAt)
+        Assertions.assertEquals(category.deletedAt, actualEntity.deletedAt)
+        Assertions.assertNull(actualEntity.deletedAt)
+    }
+
+    @Test
+    open fun givenAValidCategory_whenCallsUpdate_shouldReturnCategoryUpdated() {
+        val expectedName = "Filmes"
+        val expectedDescription = "A categoria mais assistida"
+        val expectedIsActive = true
+        val category = Category.newCategory("Film", null, expectedIsActive)
+
+        Assertions.assertEquals(0, categoryRepository.count())
+        categoryRepository.saveAndFlush(CategoryJpaEntity.from(category))
+        Assertions.assertEquals(1, categoryRepository.count())
+
+        val actualInvalidEntity = categoryRepository.findById(category.id.value).get()
+
+        Assertions.assertEquals("Film", actualInvalidEntity.name)
+        Assertions.assertNull(actualInvalidEntity.description)
+        Assertions.assertEquals(expectedIsActive, actualInvalidEntity.isActive)
+
+        val aUpdatedCategory = Category.with(category).update(expectedName, expectedDescription, expectedIsActive)
+
+        val actualCategory = categoryGateway.update(aUpdatedCategory)
+
+        Assertions.assertEquals(1, categoryRepository.count())
+        Assertions.assertEquals(category.id, actualCategory!!.id)
+        Assertions.assertEquals(expectedName, actualCategory!!.name)
+        Assertions.assertEquals(expectedDescription, actualCategory!!.description)
+        Assertions.assertEquals(expectedIsActive, actualCategory!!.isActive)
+        Assertions.assertEquals(category.createdAt, actualCategory!!.createdAt)
+        Assertions.assertTrue(category.updatedAt.isBefore(actualCategory!!.updatedAt))
+        Assertions.assertEquals(category.deletedAt, actualCategory!!.deletedAt)
+        Assertions.assertNull(actualCategory!!.deletedAt)
+
+        val actualEntity = categoryRepository.findById(category.id.value).get()
+
+        Assertions.assertEquals(category.id.value, actualEntity.id)
+        Assertions.assertEquals(expectedName, actualEntity.name)
+        Assertions.assertEquals(expectedDescription, actualEntity.description)
+        Assertions.assertEquals(expectedIsActive, actualEntity.isActive)
+        Assertions.assertEquals(category.createdAt, actualEntity.createdAt)
+        Assertions.assertTrue(category.updatedAt.isBefore(actualCategory!!.updatedAt))
         Assertions.assertEquals(category.deletedAt, actualEntity.deletedAt)
         Assertions.assertNull(actualEntity.deletedAt)
     }
