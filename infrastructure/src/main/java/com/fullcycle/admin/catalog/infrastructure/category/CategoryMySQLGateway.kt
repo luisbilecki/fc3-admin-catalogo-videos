b5b2d37c5c660de635b818fa990fa4a0947ca12f
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-06-20 12:35:04 -0300

    feat: add create method for category gateway mysql

--- infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/category/CategoryMySQLGateway.kt
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/category/CategoryMySQLGateway.kt
@@ -5,6 +5,7 @@ import com.fullcycle.admin.catalog.domain.category.CategoryGateway
 import com.fullcycle.admin.catalog.domain.category.CategoryID
 import com.fullcycle.admin.catalog.domain.category.CategorySearchQuery
 import com.fullcycle.admin.catalog.domain.pagination.Pagination
+import com.fullcycle.admin.catalog.infrastructure.category.persistence.CategoryJpaEntity
 import com.fullcycle.admin.catalog.infrastructure.category.persistence.CategoryRepository
 import org.springframework.stereotype.Service
 
@@ -12,9 +13,9 @@ import org.springframework.stereotype.Service
 @Service
 class CategoryMySQLGateway(private val repository: CategoryRepository) : CategoryGateway {
 
-    override fun create(category: Category): Category {
-        return null
-    }
+    override fun create(category: Category) = repository
+        .save(CategoryJpaEntity.from(category))
+        .toAggregate()
 
     override fun deleteById(anId: CategoryID) {}
     override fun findById(anId: CategoryID): Category? {
@@ -26,6 +27,6 @@ class CategoryMySQLGateway(private val repository: CategoryRepository) : Categor
     }
 
     override fun findAll(query: CategorySearchQuery): Pagination<Category> {
-        return null
+        return Pagination(0, 0, 0, emptyList())
     }
 }
--- infrastructure/src/test/java/com/fullcycle/admin/catalog/infrastructure/category/CategoryMySQLGatewayTest.kt
+++ infrastructure/src/test/java/com/fullcycle/admin/catalog/infrastructure/category/CategoryMySQLGatewayTest.kt
@@ -5,13 +5,17 @@ import com.fullcycle.admin.catalog.infrastructure.MySQLGatewayTest
 import com.fullcycle.admin.catalog.infrastructure.category.persistence.CategoryRepository
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 
 
 @MySQLGatewayTest
-open class CategoryMySQLGatewayTest(
-    private val categoryGateway: CategoryMySQLGateway,
-    private val categoryRepository: CategoryRepository
-) {
+open class CategoryMySQLGatewayTest {
+
+    @Autowired
+    private lateinit var categoryGateway: CategoryMySQLGateway
+
+    @Autowired
+    private lateinit var categoryRepository: CategoryRepository
 
     @Test
     fun givenAValidCategory_whenCallsCreate_shouldReturnANewCategory() {
@@ -46,4 +50,37 @@ open class CategoryMySQLGatewayTest(
         Assertions.assertNull(actualEntity.deletedAt)
     }
 
+    @Test
+    open fun givenAnInvalidCategoryName_whenCallsCreate_shouldReturnANewCategory() {
+        val expectedName = "Filmes"
+        val expectedDescription = "A categoria mais assistida"
+        val expectedIsActive = true
+
+        val category = Category.newCategory(expectedName, expectedDescription, expectedIsActive)
+
+        Assertions.assertEquals(0, categoryRepository.count())
+
+        val actualCategory = categoryGateway.create(category)
+
+        Assertions.assertEquals(1, categoryRepository.count())
+        Assertions.assertEquals(category.id, actualCategory.id)
+        Assertions.assertEquals(expectedName, actualCategory.name)
+        Assertions.assertEquals(expectedDescription, actualCategory.description)
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive)
+        Assertions.assertEquals(category.createdAt, actualCategory.createdAt)
+        Assertions.assertEquals(category.updatedAt, actualCategory.updatedAt)
+        Assertions.assertEquals(category.deletedAt, actualCategory.deletedAt)
+        Assertions.assertNull(actualCategory.deletedAt)
+
+        val actualEntity = categoryRepository.findById(category.id.value).get()
+
+        Assertions.assertEquals(category.id.value, actualEntity.id)
+        Assertions.assertEquals(expectedName, actualEntity.name)
+        Assertions.assertEquals(expectedDescription, actualEntity.description)
+        Assertions.assertEquals(expectedIsActive, actualEntity.isActive)
+        Assertions.assertEquals(category.createdAt, actualEntity.createdAt)
+        Assertions.assertEquals(category.updatedAt, actualEntity.updatedAt)
+        Assertions.assertEquals(category.deletedAt, actualEntity.deletedAt)
+        Assertions.assertNull(actualEntity.deletedAt)
+    }
 }
