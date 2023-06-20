a3457c58c1017ebeb02060e6fd0f30992ecbb0a9
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-06-20 17:02:24 -0300

    feat: implement update on category mysql gateway

--- infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/category/CategoryMySQLGateway.kt
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/category/CategoryMySQLGateway.kt
@@ -13,20 +13,20 @@ import org.springframework.stereotype.Service
 @Service
 class CategoryMySQLGateway(private val repository: CategoryRepository) : CategoryGateway {
 
-    override fun create(category: Category) = repository
-        .save(CategoryJpaEntity.from(category))
-        .toAggregate()
+    override fun create(category: Category) = save(category)
 
     override fun deleteById(anId: CategoryID) {}
     override fun findById(anId: CategoryID): Category? {
         return null
     }
 
-    override fun update(category: Category): Category? {
-        return null
-    }
+    override fun update(category: Category) = save(category)
 
     override fun findAll(query: CategorySearchQuery): Pagination<Category> {
         return Pagination(0, 0, 0, emptyList())
     }
+
+    private fun save(category: Category) = repository
+        .save(CategoryJpaEntity.from(category))
+        .toAggregate()
 }
