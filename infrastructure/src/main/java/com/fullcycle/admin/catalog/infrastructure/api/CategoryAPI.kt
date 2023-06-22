abd2d56fc29af2e86ea63ae9abfd3e5d37027180
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-06-22 15:09:16 -0300

    feat: add spring docs open api

--- infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/api/CategoryAPI.kt
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/api/CategoryAPI.kt
@@ -1,6 +1,10 @@
 package com.fullcycle.admin.catalog.infrastructure.api
 
 import com.fullcycle.admin.catalog.domain.pagination.Pagination
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -10,14 +14,27 @@ import org.springframework.web.bind.annotation.RequestParam
 
 
 @RequestMapping(value = ["categories"])
+@Tag(name = "Categories")
 interface CategoryAPI {
     @PostMapping(
         consumes = [MediaType.APPLICATION_JSON_VALUE],
         produces = [MediaType.APPLICATION_JSON_VALUE]
     )
+    @Operation(summary = "Create a new category")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "201", description = "Created successfully"),
+        ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+        ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
+    ])
     fun createCategory(): ResponseEntity<*>?
 
     @GetMapping
+    @Operation(summary = "List all categories paginated")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Listed successfully"),
+        ApiResponse(responseCode = "422", description = "A invalid parameter was received"),
+        ApiResponse(responseCode = "500", description = "An internal server error was thrown"),
+    ])
     fun listCategories(
         @RequestParam(name = "search", required = false, defaultValue = "") search: String?,
         @RequestParam(name = "page", required = false, defaultValue = "0") page: Int,
