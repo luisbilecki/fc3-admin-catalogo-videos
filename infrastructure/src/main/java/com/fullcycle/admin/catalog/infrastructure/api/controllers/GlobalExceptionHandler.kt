1df6b21206d5c262b32db39f76c61b2f07a0e538
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-06-22 15:46:28 -0300

    feat: add a global exception handler

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/api/controllers/GlobalExceptionHandler.kt
@@ -0,0 +1,25 @@
+package com.fullcycle.admin.catalog.infrastructure.api.controllers
+
+import com.fullcycle.admin.catalog.domain.exceptions.DomainException
+import com.fullcycle.admin.catalog.domain.validation.Error
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+    @ExceptionHandler(value = [DomainException::class])
+    fun handleDomainException(ex: DomainException): ResponseEntity<*> = ResponseEntity
+        .unprocessableEntity()
+        .body(APIError.from(ex))
+
+    data class APIError(val message: String?, val errors: List<Error>) {
+        companion object {
+
+            fun from(ex: DomainException): APIError {
+                return APIError(ex.message, ex.errors)
+            }
+        }
+    }
+}
--- infrastructure/src/test/java/com/fullcycle/admin/catalog/infrastructure/api/CategoryAPITest.kt
+++ infrastructure/src/test/java/com/fullcycle/admin/catalog/infrastructure/api/CategoryAPITest.kt
@@ -4,10 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fullcycle.admin.catalog.ControllerTest
 import com.fullcycle.admin.catalog.application.category.create.CreateCategoryOutput
 import com.fullcycle.admin.catalog.application.category.create.CreateCategoryUseCase
-import com.fullcycle.admin.catalog.domain.category.CategoryID
+import com.fullcycle.admin.catalog.domain.exceptions.DomainException
+import com.fullcycle.admin.catalog.domain.validation.Error
+import com.fullcycle.admin.catalog.domain.validation.handler.Notification
 import com.fullcycle.admin.catalog.infrastructure.category.models.CreateCategoryApiInput
+import io.vavr.API.Left
 import io.vavr.API.Right
-import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.*
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.`when`
 import org.mockito.kotlin.any
@@ -58,6 +61,66 @@ class CategoryAPITest @Autowired constructor(
             (Objects.equals(expectedName, cmd.name)
                     && Objects.equals(expectedDescription, cmd.description)
                     && Objects.equals(expectedIsActive, cmd.isActive))
+        })
+    }
+
+    @Test
+    @Throws(java.lang.Exception::class)
+    fun givenAInvalidName_whenCallsCreateCategory_thenShouldReturnNotification() {
+        val expectedName: String? = null
+        val expectedDescription = "A categoria mais assistida"
+        val expectedIsActive = true
+        val expectedMessage = "'name' should not be null"
+
+        val input = CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive)
+
+        `when`(createCategoryUseCase.execute(any()))
+            .thenReturn(Left(Notification.create(Error(expectedMessage))))
+
+        val request = post("/categories")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(input))
+
+        mvc.perform(request)
+            .andDo(print())
+            .andExpect(status().isUnprocessableEntity())
+            .andExpect(header().string("Location", nullValue()))
+            .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.errors", hasSize<Error>(1)))
+            .andExpect(jsonPath("$.errors[0].message", equalTo(expectedMessage)))
+
+        verify(createCategoryUseCase, times(1)).execute(argThat { cmd ->
+            expectedName == cmd.name && expectedDescription == cmd.description && expectedIsActive == cmd.isActive
+        })
+    }
+
+    @Test
+    @Throws(java.lang.Exception::class)
+    fun givenAInvalidCommand_whenCallsCreateCategory_thenShouldReturnDomainException() {
+        val expectedName: String? = null
+        val expectedDescription = "A categoria mais assistida"
+        val expectedIsActive = true
+        val expectedMessage = "'name' should not be null"
+        val input = CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive)
+
+        `when`(createCategoryUseCase.execute(any()))
+            .thenThrow(DomainException.with(Error(expectedMessage)))
+
+        val request = post("/categories")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(mapper.writeValueAsString(input))
+
+        mvc.perform(request)
+            .andDo(print())
+            .andExpect(status().isUnprocessableEntity())
+            .andExpect(header().string("Location", nullValue()))
+            .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.message", equalTo(expectedMessage)))
+            .andExpect(jsonPath("$.errors", hasSize<Error>(1)))
+            .andExpect(jsonPath("$.errors[0].message", equalTo(expectedMessage)))
+
+        verify(createCategoryUseCase, times(1)).execute(argThat { cmd ->
+            expectedName == cmd.name && expectedDescription == cmd.description && expectedIsActive == cmd.isActive
         })
     }
 }
