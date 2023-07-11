1566db572f1d43846b87278f79b7024ba2750774
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-07-10 22:44:21 -0300

    feat: e2e test to create category

--- infrastructure/src/test/java/com/fullcycle/admin/catalog/E2ETest.kt
+++ infrastructure/src/test/java/com/fullcycle/admin/catalog/E2ETest.kt
@@ -2,6 +2,7 @@ package com.fullcycle.admin.catalog
 
 import com.fullcycle.admin.catalog.infrastructure.configuration.WebServerConfig
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import java.lang.annotation.Inherited
@@ -12,4 +13,5 @@ import java.lang.annotation.Inherited
 @ActiveProfiles("test-e2e")
 @SpringBootTest(classes = [WebServerConfig::class])
 @ExtendWith(MySQLCleanUpExtension::class)
+@AutoConfigureMockMvc
 annotation class E2ETest
--- infrastructure/src/test/java/com/fullcycle/admin/catalog/e2e/category/CategoryE2ETest.kt
+++ infrastructure/src/test/java/com/fullcycle/admin/catalog/e2e/category/CategoryE2ETest.kt
@@ -1,19 +1,75 @@
 package com.fullcycle.admin.catalog.e2e.category
 
+import com.fullcycle.admin.catalog.domain.category.CategoryID
+import com.fullcycle.admin.catalog.infrastructure.category.models.CategoryResponse
+import com.fullcycle.admin.catalog.infrastructure.category.models.CreateCategoryRequest
+import com.fullcycle.admin.catalog.infrastructure.category.persistence.CategoryRepository
+import com.fullcycle.admin.catalog.infrastructure.configuration.json.Json
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
+import org.springframework.test.web.servlet.MockMvc
 import org.testcontainers.containers.MySQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+
 @Testcontainers
-class CategoryE2ETest {
+class CategoryE2ETest @Autowired constructor(
+    private val mvc: MockMvc,
+    private val categoryRepository: CategoryRepository
+) {
 
     @Test
-    fun testWorks() {
+    fun asACatalogAdminIShouldBeAbleToCreateANewCategoryWithValidValues() {
         Assertions.assertTrue(MYSQL_CONTAINER.isRunning)
+        Assertions.assertEquals(0, categoryRepository.count())
+
+        val expectedName = "Filmes"
+        val expectedDescription = "A categoria mais assistida"
+        val expectedIsActive = true
+        val actualId = givenACategory(expectedName, expectedDescription, expectedIsActive)
+        val actualCategory = retrieveACategory(actualId.value)
+
+        Assertions.assertEquals(expectedName, actualCategory.name)
+        Assertions.assertEquals(expectedDescription, actualCategory.description)
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive)
+        Assertions.assertNotNull(actualCategory.createdAt)
+        Assertions.assertNotNull(actualCategory.updatedAt)
+        Assertions.assertNull(actualCategory.deletedAt)
+    }
+
+    private fun givenACategory(aName: String, aDescription: String, isActive: Boolean): CategoryID {
+        val requestBody = CreateCategoryRequest(aName, aDescription, isActive)
+        val request = post("/categories")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(Json.writeValueAsString(requestBody))
+        val actualId = mvc.perform(request)
+            .andExpect(status().isCreated())
+            .andReturn()
+            .response.getHeader("Location")
+            .replace("/categories/", "")
+
+        return CategoryID.from(actualId)
+    }
+
+    private fun retrieveACategory(anId: String): CategoryResponse {
+        val request = get("/categories/$anId")
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+        val json = mvc.perform(request)
+            .andExpect(status().isOk())
+            .andReturn()
+            .response.contentAsString
+
+        return Json.readValue(json, CategoryResponse::class.java)
     }
 
     companion object {
