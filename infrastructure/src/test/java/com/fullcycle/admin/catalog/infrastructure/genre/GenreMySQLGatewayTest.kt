eebd2e85a489b17cead267760adf148bad9feb6c
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-09-23 20:14:23 -0300

    feat: add tests for genre update

--- infrastructure/src/test/java/com/fullcycle/admin/catalog/infrastructure/genre/GenreMySQLGatewayTest.kt
+++ infrastructure/src/test/java/com/fullcycle/admin/catalog/infrastructure/genre/GenreMySQLGatewayTest.kt
@@ -5,6 +5,7 @@ import com.fullcycle.admin.catalog.domain.category.Category
 import com.fullcycle.admin.catalog.domain.category.CategoryID
 import com.fullcycle.admin.catalog.domain.genre.Genre
 import com.fullcycle.admin.catalog.infrastructure.category.CategoryMySQLGateway
+import com.fullcycle.admin.catalog.infrastructure.genre.persistence.GenreJpaEntity
 import com.fullcycle.admin.catalog.infrastructure.genre.persistence.GenreRepository
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
@@ -86,5 +87,156 @@ open class GenreMySQLGatewayTest @Autowired constructor(
         Assertions.assertEquals(genre.updatedAt, persistedGenre.updatedAt)
         Assertions.assertEquals(genre.deletedAt, persistedGenre.deletedAt)
         Assertions.assertNull(persistedGenre.deletedAt)
+    }
+
+
+    @Test
+    open fun givenAValidGenreWithoutCategories_whenCallsUpdateGenreWithCategories_shouldPersistGenre() {
+        val movies = categoryGateway.create(Category.newCategory("Filmes", null, true))
+        val series = categoryGateway.create(Category.newCategory("Séries", null, true))
+        val expectedName = "Ação"
+        val expectedIsActive = true
+        val expectedCategories = listOf(movies!!.id, series!!.id)
+        val genre = Genre.newGenre("ac", expectedIsActive)
+        val expectedId = genre.id
+        Assertions.assertEquals(0, genreRepository.count())
+
+        genreRepository.saveAndFlush(GenreJpaEntity.from(genre))
+
+        Assertions.assertEquals("ac", genre.name)
+        Assertions.assertEquals(0, genre.categories.size)
+
+        val actualGenre = genreGateway.update(
+            Genre.with(genre)
+                .update(expectedName, expectedIsActive, expectedCategories)!!
+        )
+        Assertions.assertEquals(1, genreRepository.count())
+        Assertions.assertEquals(expectedId, actualGenre!!.id)
+        Assertions.assertEquals(expectedName, actualGenre!!.name)
+        Assertions.assertEquals(expectedIsActive, actualGenre!!.isActive)
+        Assertions.assertEquals(expectedCategories, actualGenre!!.categories)
+        Assertions.assertEquals(genre.createdAt, actualGenre!!.createdAt)
+        Assertions.assertTrue(genre.updatedAt.isBefore(actualGenre!!.updatedAt))
+        Assertions.assertEquals(genre.deletedAt, actualGenre!!.deletedAt)
+
+        val persistedGenre = genreRepository.findById(expectedId.value).get()
+        Assertions.assertEquals(expectedName, persistedGenre.name)
+        Assertions.assertEquals(expectedIsActive, persistedGenre.isActive)
+        Assertions.assertEquals(expectedCategories, persistedGenre.getCategoryIDs())
+        Assertions.assertEquals(genre.createdAt, persistedGenre.createdAt)
+        Assertions.assertTrue(genre.updatedAt.isBefore(persistedGenre.updatedAt))
+        Assertions.assertEquals(genre.deletedAt, persistedGenre.deletedAt)
+        Assertions.assertNull(persistedGenre.deletedAt)
+    }
+
+    @Test
+    open fun givenAValidGenreWithCategories_whenCallsUpdateGenreCleaningCategories_shouldPersistGenre() {
+        val movies = categoryGateway.create(Category.newCategory("Filmes", null, true))
+        val series = categoryGateway.create(Category.newCategory("Séries", null, true))
+        val expectedName = "Ação"
+        val expectedIsActive = true
+        val expectedCategories = listOf<CategoryID>()
+        val genre = Genre.newGenre("ac", expectedIsActive)
+        genre.addCategories(listOf(movies!!.id, series!!.id))
+        val expectedId = genre.id
+        Assertions.assertEquals(0, genreRepository.count())
+
+        genreRepository.saveAndFlush(GenreJpaEntity.from(genre))
+        Assertions.assertEquals("ac", genre.name)
+        Assertions.assertEquals(2, genre.categories.size)
+        val actualGenre = genreGateway.update(
+            Genre.with(genre)
+                .update(expectedName, expectedIsActive, expectedCategories)!!
+        )
+        Assertions.assertEquals(1, genreRepository.count())
+        Assertions.assertEquals(expectedId, actualGenre!!.id)
+        Assertions.assertEquals(expectedName, actualGenre!!.name)
+        Assertions.assertEquals(expectedIsActive, actualGenre!!.isActive)
+        Assertions.assertEquals(expectedCategories, actualGenre!!.categories)
+        Assertions.assertEquals(genre.createdAt, actualGenre!!.createdAt)
+        Assertions.assertTrue(genre.updatedAt.isBefore(actualGenre!!.updatedAt))
+        Assertions.assertEquals(genre.deletedAt, actualGenre!!.deletedAt)
+
+        val persistedGenre = genreRepository.findById(expectedId.value).get()
+        Assertions.assertEquals(expectedName, persistedGenre.name)
+        Assertions.assertEquals(expectedIsActive, persistedGenre.isActive)
+        Assertions.assertEquals(expectedCategories, persistedGenre.getCategoryIDs())
+        Assertions.assertEquals(genre.createdAt, persistedGenre.createdAt)
+        Assertions.assertTrue(genre.updatedAt.isBefore(persistedGenre.updatedAt))
+        Assertions.assertEquals(genre.deletedAt, persistedGenre.deletedAt)
+        Assertions.assertNull(persistedGenre.deletedAt)
+    }
+
+    @Test
+    open fun givenAValidGenreInactive_whenCallsUpdateGenreActivating_shouldPersistGenre() {
+        val expectedName = "Ação"
+        val expectedIsActive = true
+        val expectedCategories = listOf<CategoryID>()
+        val genre = Genre.newGenre(expectedName, false)
+        val expectedId = genre.id
+        Assertions.assertEquals(0, genreRepository.count())
+
+        genreRepository.saveAndFlush(GenreJpaEntity.from(genre))
+        Assertions.assertFalse(genre.isActive)
+        Assertions.assertNotNull(genre.deletedAt)
+
+        val actualGenre = genreGateway.update(
+            Genre.with(genre)
+                .update(expectedName, expectedIsActive, expectedCategories)!!
+        )
+        Assertions.assertEquals(1, genreRepository.count())
+        Assertions.assertEquals(expectedId, actualGenre!!.id)
+        Assertions.assertEquals(expectedName, actualGenre!!.name)
+        Assertions.assertEquals(expectedIsActive, actualGenre!!.isActive)
+        Assertions.assertEquals(expectedCategories, actualGenre!!.categories)
+        Assertions.assertEquals(genre.createdAt, actualGenre!!.createdAt)
+        Assertions.assertTrue(genre.updatedAt.isBefore(actualGenre!!.updatedAt))
+        Assertions.assertNull(genre.deletedAt)
+
+        val persistedGenre = genreRepository.findById(expectedId.value).get()
+        Assertions.assertEquals(expectedName, persistedGenre.name)
+        Assertions.assertEquals(expectedIsActive, persistedGenre.isActive)
+        Assertions.assertEquals(expectedCategories, persistedGenre.getCategoryIDs())
+        Assertions.assertEquals(genre.createdAt, persistedGenre.createdAt)
+        Assertions.assertTrue(genre.updatedAt.isBefore(persistedGenre.updatedAt))
+        Assertions.assertEquals(genre.deletedAt, persistedGenre.deletedAt)
+        Assertions.assertNull(persistedGenre.deletedAt)
+    }
+
+    @Test
+    open fun givenAValidGenreActive_whenCallsUpdateGenreInactivating_shouldPersistGenre() {
+        val expectedName = "Ação"
+        val expectedIsActive = false
+        val expectedCategories = listOf<CategoryID>()
+        val genre = Genre.newGenre(expectedName, true)
+        val expectedId = genre.id
+        Assertions.assertEquals(0, genreRepository.count())
+
+        genreRepository.saveAndFlush(GenreJpaEntity.from(genre))
+
+        Assertions.assertTrue(genre.isActive)
+        Assertions.assertNull(genre.deletedAt)
+
+        val actualGenre = genreGateway.update(
+            Genre.with(genre)
+                .update(expectedName, expectedIsActive, expectedCategories)!!
+        )
+        Assertions.assertEquals(1, genreRepository.count())
+        Assertions.assertEquals(expectedId, actualGenre!!.id)
+        Assertions.assertEquals(expectedName, actualGenre!!.name)
+        Assertions.assertEquals(expectedIsActive, actualGenre!!.isActive)
+        Assertions.assertEquals(expectedCategories, actualGenre!!.categories)
+        Assertions.assertEquals(genre.createdAt, actualGenre!!.createdAt)
+        Assertions.assertTrue(genre.updatedAt.isBefore(actualGenre!!.updatedAt))
+        Assertions.assertNotNull(genre.deletedAt)
+
+        val persistedGenre = genreRepository.findById(expectedId.value).get()
+        Assertions.assertEquals(expectedName, persistedGenre.name)
+        Assertions.assertEquals(expectedIsActive, persistedGenre.isActive)
+        Assertions.assertEquals(expectedCategories, persistedGenre.getCategoryIDs())
+        Assertions.assertEquals(genre.createdAt, persistedGenre.createdAt)
+        Assertions.assertTrue(genre.updatedAt.isBefore(persistedGenre.updatedAt))
+        Assertions.assertEquals(genre.deletedAt, persistedGenre.deletedAt)
+        Assertions.assertNotNull(persistedGenre.deletedAt)
     }
 }
